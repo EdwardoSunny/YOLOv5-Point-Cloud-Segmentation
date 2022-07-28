27616cf4c5a0e229e09aaf2f1c89e87b0de5b3b4
EdwardoSunny <edward.sun2015@gmail.com>
2022-07-28 05:57:00 -0400

    segment tennis ball works

--- src/yolov5_segment/src/plysee.py
+++ src/yolov5_segment/src/plysee.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from matplotlib import cm as plt
 
-pcd = pcd = o3d.io.read_point_cloud('tennisball.ply')
+pcd  = o3d.io.read_point_cloud('tennisball.ply')
 # o3d.visualization.draw_geometries([pcd])
 
 with o3d.utility.VerbosityContextManager(
@@ -11,13 +11,16 @@ with o3d.utility.VerbosityContextManager(
 # 17.5, clusters:  2     5, clusters: 4
     labels = np.array(
         pcd.cluster_dbscan(eps=5, min_points=10, print_progress=True))
-
-test = pcd.cluster_dbscan(eps=5, min_points=10, print_progress=True)
-print(type(test))
+# zero baesd labels
 
 max_label = labels.max()
+print(max_label)
+cluster_closest_idx = np.where(labels == 3)[0].tolist()
+pcd_cut = pcd.select_by_index(cluster_closest_idx)
+
 print(f"point cloud has {max_label + 1} clusters")
 colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
 colors[labels < 0] = 0
 pcd.colors = o3d.utility.Vector3dVector(colors[:, :3])
 o3d.visualization.draw_geometries([pcd])
+o3d.visualization.draw_geometries([pcd_cut])
